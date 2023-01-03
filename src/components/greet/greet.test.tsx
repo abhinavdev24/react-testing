56732b02895f4ae4f73a0207cf596a271757501a
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2023-01-03 21:51:14 +0530

    10. Test Driven Development

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,19 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component
+ * It should render hello followed by the name
+ */
+
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
 test("Greet renders correctly", () => {
   render(<Greet />);
   const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Abhinav" />);
+  const textElement = screen.getByText("Hello Abhinav");
   expect(textElement).toBeInTheDocument();
 });
